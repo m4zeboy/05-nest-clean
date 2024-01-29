fd73ef054e7cb66fcf252410f1ecb536ebb3ac36
m4zeboy <moises775azevedo@gmail.com>
2024-01-29 11:28:41 -0300

    Conversa entre camadas (Mappers)

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,11 +2,18 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaQuestionMapper } from '../../maappers/prisma-question-mapper'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private prisma: PrismaService) {}
   async findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+    const question = await this.prisma.question.findUnique({ where: { id } })
+    if (!question) {
+      return null
+    }
+    return PrismaQuestionMapper.toDomain(question)
   }
 
   async findBySlug(slug: string): Promise<Question | null> {

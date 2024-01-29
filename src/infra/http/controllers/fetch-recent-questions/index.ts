66a98ca97790b972cec7236f4a10d56f879db421
m4zeboy <moises775azevedo@gmail.com>
2024-01-29 17:46:10 -0300

    Presenter de perguntas

--- src/infra/http/controllers/fetch-recent-questions/index.ts
+++ src/infra/http/controllers/fetch-recent-questions/index.ts
@@ -7,6 +7,7 @@ import {
 import { ZodValidationPipe } from '../../pipes/zod-validation-pipe'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { QuestionPresenter } from '../../presenters/question.presenter'
 
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
 
@@ -17,9 +18,14 @@ export class FetchRecentQuestionsController {
   @Get()
   async handle(@Query(queryValidationPipe) query: PageQueryParamSchema) {
     const page = query.page ?? 1
-    const questions = await this.fetchRecentQuestions.execute({
+    const result = await this.fetchRecentQuestions.execute({
       page,
     })
-    return { questions }
+    if (result.isFailure()) {
+      throw new Error()
+    }
+    const questions = result.value.questions
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
   }
 }

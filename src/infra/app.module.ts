6081617c04bf7ac0e4c04ff405f0523cbb88638a
m4zeboy <moises775azevedo@gmail.com>
2024-02-06 17:36:33 -0300

    Criando EnvModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { envSchema } from './env'
+import { envSchema } from './env/env'
 import { AuthModule } from './auth/auth.module'
-import { JwtStrategy } from './auth/jwt.strategy'
 import { HttpModule } from './http/http.module'
+import { EnvModule } from './env/env.module'
 
 @Module({
   imports: [
@@ -13,7 +13,7 @@ import { HttpModule } from './http/http.module'
     }),
     AuthModule,
     HttpModule,
+    EnvModule,
   ],
-  providers: [JwtStrategy],
 })
 export class AppModule {}
